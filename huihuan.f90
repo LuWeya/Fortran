4871db1843f089e1967c238f6b33acf7a202ba02
LuWeya <lwy201408@outlook.com>
2018-04-12 12:44:10 +0800

    ff

--- huihuan.f90
+++ huihuan.f90
@@ -2,10 +2,12 @@ program main
 include 'mpif.h'
 character* (MPI_MAX_PROCESSOR_NAME)processor_name
 integer myid, numprocs, namelen, rc, ierr
-	call MPI_INIT(ierr)  
-	call MPI_COMM_RANK(MPI_COMM_WORLD,myid,ierr)  
-	call MPI_COMM_SIZE(MPI_COMM_WORLD,numprocs,ierr)
-	call MPI_GET_PROCESSOR_NAME(processor_name,namelen,ierr)
+integer:: count_send,tag, mydata(2),node
+
+call MPI_INIT(ierr)  
+call MPI_COMM_RANK(MPI_COMM_WORLD,myid,ierr)  
+call MPI_COMM_SIZE(MPI_COMM_WORLD,numprocs,ierr)
+call MPI_GET_PROCESSOR_NAME(processor_name,namelen,ierr)
 write(*,10) myid,numprocs,processor_name
 10 FORMAT('Hello,World!Process',I2,' of ',I1,' on ',10A)
 call MPI_Barrier(MPI_COMM_WORLD,ierr)
@@ -16,31 +18,28 @@ if(numprocs<2) then
     call MPI_Abort(MPI_COMM_WORLD,1,ierr)
 endif
 
-call hello()
+count_send=2  ! the number of data you send
+tag=99        ! tag
+
+if(myid==0) then
+    write(*,*)'Hello test from all to all.'
+end if 
+do node=0,numprocs-1   ! send to each one (except itself)
+    if(node/=myid) then
+        mydata(1)=node
+        mydata(2)=myid
+        call MPI_SEND(mydata(1),count_send,MPI_INTEGER,node,tag,MPI_COMM_WORLD,ierr)
+        write(*,*) myid,' send hello to ',node
+        call MPI_RECV(mydata(2),count_send,MPI_INTEGER,node,tag,MPI_COMM_WORLD,iSTATUS,ierr)
+        write(*,*) myid,' receive hello form',node
+        if((mydata(1)/=node).or.(mydata(2)/=myid)) then
+            write(*,*)'error!'
+        end if 
+    end if
+end do 
+
+write(*,*) myid,'  data is ',mydata
 
 !--------------------------------------------------
 call MPI_FINALIZE()
-end
-
-! send hello to everyone and receive hello from everyone.
-subroutine hello()
-implicit none
-integer:: rank, nproc,ierr,node,count_send,tag,data1,data2
-integer::iSTATUS(MPI_STATUS_SIZE)
-call MPI_COMM_RANK(MPI_COMM_WORLD,rank,ierr)
-call MPI_COMM_SIZE(MPI_COMM_WORLD,nproc,ierr)
-count_send=1  ! the number of data you send
-tag=99        ! tag
-data1=1
-data2=0
-if(rank==0) then
-    write(*,*)'Hello test from all to all.'
-end if 
-do node=0,nproc-1   ! send to each one (except itself)
-    if(node/=rank) then
-        call MPI_SEND(data1,count_send,MPI_INTEGER,node,tag,MPI_COMM_WORLD,ierr)
-        call MPI_SEND(data2,count_send,MPI_INTEGER,node,tag,MPI_COMM_WORLD,iSTATUS,ierr)
-        write(*,*) rank,' receive hellon form',node
-    end if
-end do 
-end subroutine hello
+end program
