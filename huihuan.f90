21706b45b3b90c07c06ef88b3f74d7ef565e3cb3
LuWeya <lwy201408@outlook.com>
2018-04-12 11:02:46 +0800

    dd

--- huihuan.f90
+++ huihuan.f90
@@ -11,6 +11,7 @@ write(*,10) myid,numprocs,processor_name
 10 FORMAT('Hello,World!Process',I2,' of ',I1,' on ',10A)
 call MPI_Barrier(MPI_COMM_WORLD,ierr)
 !----------------------正文部分--------------------
+! judge if number of processors is less than 2
 if(numprocs<2) then
     write(*,*)'The number of processor is less than 2.'
     call MPI_Abort(MPI_COMM_WORLD,1,ierr)
@@ -22,21 +23,24 @@ call hello()
 call MPI_FINALIZE()
 end
 
+! send hello to everyone and receive hello from everyone.
+
 subroutine hello()
+include 'mpi.h'
 implicit none
 integer:: rank, nproc,ierr,node,count_send,tag,data1,data2
 integer::iSTATUS(MPI_STATUS_SIZE)
 call MPI_COMM_RANK(MPI_COMM_WORLD,rank,ierr)
 call MPI_COMM_SIZE(MPI_COMM_WORLD,nproc,ierr)
-count_send=1
-tag=99
+count_send=1  ! the number of data you send
+tag=99        ! tag
 data1=1
 data2=0
-if(rank==0)
+if(rank==0) then
     write(*,*)'Hello test from all to all.'
 end if 
-do node=0,nproc-1
-    if(node/=rank)
+do node=0,nproc-1   ! send to each one (except itself)
+    if(node/=rank) then 
         call MPI_SEND(data1,count_send,MPI_INTEGER,node,tag,MPI_COMM_WORLD,ierr)
         call MPI_SEND(data2,count_send,MPI_INTEGER,node,tag,MPI_COMM_WORLD,iSTATUS,ierr)
         write(*,*) rank,' receive hellon form',node
